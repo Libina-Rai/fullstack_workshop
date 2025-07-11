af2d5c438baf547fa7ebddea4c2c12d8b39c5c6c
Libina-Rai <libinarai@gmail.com>
2025-07-11 14:27:10 +0545

    feat: fetch notes from backend and update state

--- Part2/notes-app/src/App.jsx
+++ Part2/notes-app/src/App.jsx
@@ -1,14 +1,22 @@
 import { useState, useEffect } from "react";
 import Note from "./components/Note";
+import axios from "axios";
 
 const App = (props) => {
-  const [notes, setNotes] = useState(props.notes);
+  const [notes, setNotes] = useState([]);
   const [newNote, setNewNote] = useState("");
   const [showAll, setShowAll] = useState(true);
 
   useEffect(() => {
-    console.log("hello");// log when newNote state changes
-  }, [newNote]);
+    //1. get data from backend server
+    let myAxiosPromise = axios.get("http://localhost:3001/notes");
+    myAxiosPromise.then((myResult) => {
+      console.log("return promise");
+      console.dir(myResult.data);
+      //2. put the data into notes state
+      setNotes(myResult.data);
+    });
+  }, []);
 
   const notesToShow = notes.filter((note) => (showAll ? true : note.important));
 
