c20c6d2cb3e43ca03de7a6a8174ec8294e0c184c
Libina-Rai <libinarai@gmail.com>
2025-07-24 16:41:24 +0545

    feat: set up basic HTTP server

---
+++ part3/notes-server/index.js
@@ -0,0 +1,12 @@
+console.log("Hello there");
+
+const http = require("http");
+
+const app = http.createServer((request, response) =>{
+  response.writeHead(200, { "content-Type": "text/html" });
+  response.end("<h1>Hello World</h1>");
+})
+
+const PORT = 3001;
+app.listen(PORT);
+console.log(`Server running on port ${PORT}`);
