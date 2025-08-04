1bc8b7124f1c764249c68f26e13e18088384a952
Libina-Rai <libinarai@gmail.com>
2025-08-04 17:24:24 +0545

    feat: use notes-server instead of json-server and serve frontend from backend

--- part3/notes-server/index.js
+++ part3/notes-server/index.js
@@ -1,14 +1,17 @@
-const express = require('express')
-const app = express()
+const express = require("express");
+const app = express();
+const cors = require("cors");
 
 app.use(express.json());
+app.use(cors());
+app.use(express.static("dist"));
 
 const requestLogger = (request, response, next) => {
-  console.log('Method:', request.method)
-  console.log('Path:  ', request.path)
-  console.log('Body:  ', request.body)
-  console.log('we just wrote this code')
-  next()
+  console.log("Method:", request.method);
+  console.log("Path:  ", request.path);
+  console.log("Body:  ", request.body);
+  console.log("we just wrote this code");
+  next();
 };
 
 app.use(requestLogger);
@@ -17,52 +20,51 @@ let notes = [
   {
     id: "1",
     content: "HTML is easy",
-    important: true
+    important: true,
   },
   {
     id: "2",
     content: "Browser can execute only JavaScript",
-    important: false
+    important: false,
   },
   {
     id: "3",
     content: "GET and POST are the most important methods of HTTP protocol",
-    important: true
-  }
-]
+    important: true,
+  },
+];
 
-app.get("/", (request, response) =>{
-  response.send("<h2>Hello Chai Pilo</h2>");
-})
 
-app.get("/api/notes", (request, response) =>{
+
+app.get("/api/notes", (request, response) => {
   response.json(notes);
-})
+});
 
-app.get("/api/notes/:id", (request, response) =>{
-  const myId =(request.params.id);
+app.get("/api/notes/:id", (request, response) => {
+  const myId = request.params.id;
   const myNote = notes.find((note) => note.id === myId);
-  if(myNote){
+  if (myNote) {
     response.json(myNote);
   } else {
     response.status(404).send(`Note with id ${myId} not found`);
   }
-  
-})
-
-app.delete("/api/notes/:id", (request, response) =>{
-  const myId =(request.params.id);
-  notes = notes.filter((note) => note.id !== myId);
-  
-    response.status(404).send(`The note at id ${myId} has been deleted`);
 });
 
-app.post("/api/notes", (request, response) =>{
+app.delete("/api/notes/:id", (request, response) => {
+  const myId = request.params.id;
+  notes = notes.filter((note) => note.id !== myId);
+
+  response.status(404).send(`The note at id ${myId} has been deleted`);
+});
+
+app.post("/api/notes", (request, response) => {
   const myNewPost = request.body;
   myNewPost.id = notes.length + 1;
   notes.push(myNewPost);
   response.status(201).json(myNewPost);
 });
+
+
 
 app.use((request, response, next) => {
   response.status(404).send("no code available to handle this request");
