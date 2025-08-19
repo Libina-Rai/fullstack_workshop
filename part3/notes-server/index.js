7091e8bf953ebbd330abb6bfa9b4119310dad510
Libina-Rai <libinarai@gmail.com>
2025-08-19 12:08:28 +0545

    feat: enhance note retrieval and error handling in API endpoints

--- part3/notes-server/index.js
+++ part3/notes-server/index.js
@@ -47,43 +47,75 @@ app.get("/api/notes", (request, response) => {
 });
 
 app.get("/api/notes/:id", (request, response) => {
-  const myId = request.params.id;
-  const myNote = notes.find((note) => note.id === myId);
-  if (myNote) {
-    response.json(myNote);
-  } else {
-    response.status(404).send(`Note with id ${myId} not found`);
-  }
+  Note.findById(request.params.id)
+    .then((result) => {
+      if (result) {
+        response.json(result);
+      } else {
+        response.status(404).send(`There are no notes at ${request.params.id}`);
+      }
+    })
+    .catch((e) => {
+      next(e);
+      // response.status(500).send(`${request.params.id} is not in the required format`);
+    });
 });
 
-app.delete("/api/notes/:id", (request, response) => {
-  const myId = request.params.id;
-  notes = notes.filter((note) => note.id !== myId);
+app.put("/api/notes/:id", (request, response, next) => {
+  const body = request.body;
 
-  response.status(404).send(`The note at id ${myId} has been deleted`);
+  const note = {
+    content: body.content,
+    important: body.important,
+  };
+
+  Note.findByIdAndUpdate(request.params.id, note, { new: true })
+    .then((updatedNote) => {
+      response.json(updatedNote);
+    })
+    .catch((error) => next(error));
+});
+
+app.delete("/api/notes/:id", (request, response, next) => {
+  Note.findByIdAndRemove(request.params.id)
+    .then((result) => {
+      response.status(204).end();
+    })
+    .catch((error) => next(error));
 });
 
 app.post("/api/notes", (request, response) => {
-  const myNewPost = request.body;
-  myNewPost.id = notes.length + 1;
-  notes.push(myNewPost);
-  response.status(201).json(myNewPost);
+  const body = request.body;
 
-  //   const note = new Note({
-  //   content: 'HTML is easy',
-  //   important: true,
-  // })
+  if (body.content === undefined) {
+    return response.status(400).json({ error: "content missing" });
+  }
 
-  // note.save().then(result => {
-  //   console.log('note saved!')
-  //   mongoose.connection.close()
-  // })
+  const note = new Note({
+    content: body.content,
+    important: body.important || false,
+  });
+
+  note.save().then((savedNote) => {
+    response.json(savedNote);
+  });
 });
 
 app.use((request, response, next) => {
   response.status(404).send("no code available to handle this request");
 });
 
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message);
+
+  if (error.name === "CastError") {
+    return response.status(400).send({ error: "malformatted id" });
+  }
+  next(error);
+};
+
+//this has to be the last loaded middleware.
+app.use(errorHandler);
 
 app.listen(process.env.PORT);
 console.log(`Server running on port ${process.env.PORT}`);
