9922f6a75eed48b1102606ccb101eb62f978f4a9
Libina-Rai <libinarai@gmail.com>
2025-08-25 21:50:46 +0545

    feat: enhance API service methods to return data directly

--- Part2/notes-app/src/services/notes.js
+++ Part2/notes-app/src/services/notes.js
@@ -1,17 +1,20 @@
 import axios from "axios";
 
-const baseUrl = "/api/notes";
+const baseUrl =
+  import.meta.env.MODE === "production"
+    ? "/api/notes" // On Render: same origin
+    : "http://localhost:3001/api/notes"; // On local: backend
 
 const getAll = () => {
   return axios.get(baseUrl).then((result) => result.data);
 };
 
 const create = (note) => {
-  return axios.post(baseUrl, note);
+  return axios.post(baseUrl, note).then((result) => result.data);
 };
 
 const update = (id, updatedNote) => {
-  return axios.put(`${baseUrl}/${id}`, updatedNote);
+  return axios.put(`${baseUrl}/${id}`, updatedNote).then((result) => result.data);
 };
 
 export default { create, getAll, update };
