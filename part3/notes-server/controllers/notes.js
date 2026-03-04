af3c9ddf37aed89f3cd8606a51f78972b2253cff
Libina-Rai <libinarai@gmail.com>
2026-03-05 01:34:24 +0545

    feat: implement user login functionality with JWT authentication

--- part3/notes-server/controllers/notes.js
+++ part3/notes-server/controllers/notes.js
@@ -1,6 +1,7 @@
 const Note = require("../model/note");
 const notesRouter = require("express").Router();
 const User = require("../model/user");
+const jwt = require("jsonwebtoken");
 
 // GET all notes
 notesRouter.get("/", async (request, response, next) => {
@@ -60,19 +61,35 @@ notesRouter.delete("/:noteid", async (request, response, next) => {
 });
 
 // CREATE note
+const getTokenFrom=(request) => {
+  const authorization = request.get("Authorization");
+  console.log("authorization is", authorization);
+  if(authorization && authorization.startsWith("Bearer ")){
+    const token = authorization.replace("Bearer ", "");
+    return token;
+  }
+  return null;
+}
 notesRouter.post("/", async (request, response, next) => {
+  
   try {
+    console.log("extracted token:", getTokenFrom(request));
+    const decodedToken = jwt.verify(getTokenFrom(request), process.env.SECRET);
+    console.log("decoded token is", decodedToken);
+    const user = await User.findById(decodedToken.id);
+    console.log("user is", user);
+
+    if(!user){
+      return response.status(401).json({error: "userId is missing or invalid"});
+    }
     const body = request.body;
 
-    if (!body.content) {
-      return response.status(400).json({ error: "content missing" });
-    }
-    const user = await User.findById(body.userId);
     const note = new Note({
       content: body.content,
       important: body.important || false,
       user: user.id,
     });
+
     const savedNote = await note.save();
     user.notes = user.notes.concat(savedNote.id);
     await user.save();
